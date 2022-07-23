34d0e0d15bee1c7fbdd5d242db682925e3cee61e
05KBS23 <chagall2582@naver.com>
2022-07-23 21:58:32 +0900

    [week3] django 과제(진행중)

--- python-basic/programmers/week3/django/webproj/homepage/views.py
+++ python-basic/programmers/week3/django/webproj/homepage/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import HttpResponse, render
 
 # Create your views here.
-def index(request): #어느경로로와야 인덱스 함수를 사용하는가?
+def index(request): #어느경로로 와야 인덱스 함수를 사용하는가? 리퀘스트가 인자로 주어진다.
     return HttpResponse("<h1>Hello World</h1>") #리스폰스에는 에치엠엘 마크업 랭귀지 적용이 가능하다.
+    render(request, '.html', {})
+#이 뷰의 역할? 어떤 요청이 왔을 떄 "Hello world"라는 리스폰스를 주는 것
+
+#템플릿? : 모델과 뷰와는 다르게 파이썬 파일을 만들지 않고 새로운 html파일을 만드는 것
+# 이 파일을 관리하는 법은 여러가지가 있는데, 각 앱의  디렉토리에서 관리할 수 있음
+목적에 따라 디렉토리 구조는 상이할 수 있음.
+
+
--- python-basic/programmers/week3/django/webproj/webproj/urls.py
+++ python-basic/programmers/week3/django/webproj/webproj/urls.py
@@ -19,8 +19,13 @@ from django.contrib import admin
 from django.urls import path
 #위에는 원래 없었음
 from homepage.views import index
+from homepage.views import index
+#여기 모듈에는 뷰에 대한 정보가 없기에, 그 정보를 불러와야한다. 그 폴더는 view.py에 있기 때문에
+# 위 같은 함수를 요 경로를 들어오면 index함수를 실행하라고 명시.
 
 urlpatterns = [
-    path('',index), #127.0.0.1
+    path('', index), #127.0.0.1
     path('admin/', admin.site.urls),
 ]
+#이미 어드민에 요청에 대해 리스폰스를 주라는 로직이 있는데
+#다음과 같은 로직을 설정한다. 아무런 경로를 설정하지 않으면 기본 #127.0.0.1이다.
